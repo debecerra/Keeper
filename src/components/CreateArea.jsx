458f2995c2df0bb2321954b5ec6e8160cc44e841
Diego Becerra <Diego.E.Becerra@outlook.com>
2021-01-04 15:27:33 -0700

    Initial commit and push to GitHub pages

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const [active, setActive] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    setActive(false);
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form
+        onFocus={() => {
+          setActive(true);
+        }}
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+        className="create-note"
+      >
+        {active && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={active ? "3" : "1"}
+        />
+
+        <Zoom in={active}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
