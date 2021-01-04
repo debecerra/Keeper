5f332ca3c610bd5ee25463e4259555af38a4bba5
Diego Becerra <Diego.E.Becerra@outlook.com>
2021-01-04 15:10:01 -0700

    Updates

---
+++ precache-manifest.39f4d7e2b31260a4faef7a5a5d8b63f7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600cf1402f538a85818ccc75284f4b49",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "7c56644bdd726f2ef05a",
+    "url": "/Keeper/static/js/2.47e61ea9.chunk.js"
+  },
+  {
+    "revision": "0451499ec29755a6c677",
+    "url": "/Keeper/static/js/main.534c642e.chunk.js"
+  },
+  {
+    "revision": "624a019943f6d16a0069",
+    "url": "/Keeper/static/js/runtime-main.bbbc4cd4.js"
+  }
+]);
